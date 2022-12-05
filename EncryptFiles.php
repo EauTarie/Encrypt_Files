c399f49836ac355a34761d2ef4c8a5889c8063a7
28010-55-08 <28010-55-08@exchange.ad.afpanet>
2022-12-05 16:53:51 +0100

    A voir en rentrant

--- EncryptFiles.php
+++ EncryptFiles.php
@@ -2,7 +2,19 @@
 
 namespace App\Service;
 
+define('FILE_ENCRYPTION_BLOCKS', 10000);
 class EncryptFiles
 {
-    
+    public function;
+    /**
+     * @param $source   |   Path of the UNENCRYPTED FILE
+     * @param $dest     |   Path of the ENCRYPTED FILE to CREATED
+     * @param $key      |   Encryption KEY
+     */
+    public function encryptFile($source, $dest, $key)
+    {
+        $cipher = 'aes-256-cbc';
+        $ivLenght = openssl_ciper_iv_length($cipher);
+        $iv = openssl_random_pseudo_bytes($ivLenght);
+    }
 }
